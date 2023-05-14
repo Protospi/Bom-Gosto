237443b5c25207767375a612e20a4a2223ce05f2
Pedro Loes <pedro.loes@guide121.com>
2023-05-13 22:44:51 -0300

    Change back-end files

--- littlelemon/asgi.py
+++ littlelemon/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for littlelemon project.
+ASGI config for bomgosto project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- littlelemon/wsgi.py
+++ littlelemon/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for littlelemon project.
+WSGI config for bomgosto project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
